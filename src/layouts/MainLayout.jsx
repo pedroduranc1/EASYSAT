5ae4d2f1165dbad914e0998408c141bd257973ad
pedroduranc1 <61203515+pedroduranc1@users.noreply.github.com>
2023-07-10 14:17:13 -0400

    sdsd

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,7 +4,7 @@ import { Navbar } from "../components/Navbar";
 
 export const MainLayout = ({ children }) => {
   return (
-    <div className="flex flex-col bg-slate-100 w-screen">
+    <div className="flex flex-col bg-slate-100 w-full">
       <Navbar/>
       <div className="max-w-6xl mx-auto w-full">{children}</div>
     </div>
